849be10d95500e142195f550b7d2f2ba54e440d9
bhaktibuana <bhaktibuana19@gmail.com>
2022-02-17 15:40:07 +0700

    create controller for login service

---
+++ app/controllers/login.controller.js
@@ -0,0 +1,47 @@
+const loginModel = require("../models/login.model");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+require("dotenv").config();
+
+const login = (req, res) => {
+  const { phone_number, password } = req.body;
+
+  loginModel.signin(
+    [phone_number, passwordHash(password)],
+    (error, results) => {
+      if (error) {
+        res.status(500).json(error);
+      } else {
+        if (results.length < 1) {
+          res.status(400).json({
+            message: "Login failed, wrong phone number or password",
+          });
+        } else {
+          const payload = {
+            id: results[0].id,
+            name: results[0].name,
+            phone_number: results[0].phone_number,
+            address: results[0].address,
+          };
+
+          const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+            expiresIn: "10s",
+          });
+
+          res.status(200).json({
+            message: "Login success",
+            data: { token: token },
+          });
+        }
+      }
+    }
+  );
+};
+
+const passwordHash = (password) => {
+  return crypto.createHash("sha256").update(password).digest("hex");
+};
+
+module.exports = {
+  login,
+};
