5665e5e31d438867d186b1107236ba2140463004
bhaktibuana <bhaktibuana19@gmail.com>
2022-02-17 08:22:01 +0700

    create merchant service

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const productRoutes = require("../controllers/product.controller");
+const merchantRoutes = require("../controllers/merchant.controller");
 const productMiddleware = require("../middlewares/product.middleware");
+const merchantMiddleware = require("../middlewares/merchant.middleware");
 
 const router = express.Router();
 
@@ -10,10 +12,18 @@ router.get("/", (req, res) => {
   });
 });
 
+// product router
 router.get("/product", productRoutes.getListProduct);
 router.post("/product", productMiddleware.productValidation, productRoutes.addProduct);
 router.put("/product/:id", productMiddleware.productValidation, productRoutes.updateProduct);
 router.put("/product/softDelete/:id", productRoutes.softDeleteProduct);
 router.delete("/product/:id", productRoutes.deleteProduct);
+
+// merchant router
+router.post("/merchant", merchantMiddleware.createValidation, merchantRoutes.registerMerchant);
+router.put("/merchant/:id", merchantMiddleware.updateValidation, merchantRoutes.updateMerchant);
+router.put("/merchant/updatePassword/:id", merchantMiddleware.updatePasswordValidation, merchantRoutes.updateMerchantPassword);
+router.put("/merchant/softDelete/:id", merchantMiddleware.deleteValidaiton, merchantRoutes.softDeleteMerchant);
+router.delete("/merchant/:id", merchantMiddleware.deleteValidaiton, merchantRoutes.deleteMerchant);
 
 module.exports = router;
