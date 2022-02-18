bc99ed9a8ecb57a39ec743226d0d680a9f57a746
bhaktibuana <bhaktibuana19@gmail.com>
2022-02-18 14:06:18 +0700

    create find merchant by phone numbber query

--- app/models/login.model.js
+++ app/models/login.model.js
@@ -5,6 +5,12 @@ const signin = (params, callback) => {
   db.query(selectQuery, params, callback);
 };
 
+const findByPhoneNumber = (params, callback) => {
+  const selectQuery = `SELECT COUNT(*) AS count FROM merchant WHERE phone_number = ?;`;
+  db.query(selectQuery, params, callback);
+};
+
 module.exports = {
   signin,
+  findByPhoneNumber,
 };
